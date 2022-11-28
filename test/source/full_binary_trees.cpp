102e39e899a3707ca7f74ea635828aaced571e4c
Matan Levy <levymatanlevy@gmail.com>
2022-11-28 20:38:26 +0200

    feat(full_binary_trees): Initialize new nodule

---
+++ test/source/full_binary_trees.cpp
@@ -0,0 +1,11 @@
+#include <doctest/doctest.h>
+#include <full_binary_trees/full_binary_trees.h>
+
+
+TEST_CASE("Count the Number of Full Binary Trees")
+{
+    using namespace full_binary_trees;
+    MESSAGE("This a little more info on the test");
+    full_binary_trees_function();
+    CHECK(2 == 2);
+}
