640fa92669cbc0208e54b92c27abdbde2255c384
Matan Levy <levymatanlevy@gmail.com>
2022-11-28 08:56:39 +0000

    fix(test): remove version testing
    I am all setup. Each time we compile we will have a different version
    based on the latest git tag.

--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -18,8 +18,8 @@ TEST_CASE("Greeter")
     CHECK(greeter.greet(LanguageCode::HEB) == "Shalom Tests!");
 }
 
-TEST_CASE("Greeter version")
-{
-    CHECK(std::string(GEEKS4GEEKS_VERSION) == std::string("v0.9.2"));
-    static_assert(std::string_view(GEEKS4GEEKS_VERSION) == std::string_view("v0.9.2"));
-}
+// TEST_CASE("Greeter version")
+// {
+//     CHECK(std::string(GEEKS4GEEKS_VERSION) == std::string("v0.9.2"));
+//     static_assert(std::string_view(GEEKS4GEEKS_VERSION) == std::string_view("v0.9.2"));
+// }
