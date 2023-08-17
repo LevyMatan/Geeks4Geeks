3bc0f38c0b4720c05a74d7b836e2b37c70a6f71c
Matan Levy <levymatanlevy@gmail.com>
2023-08-17 09:46:18 +0000

    init next_smallest_palindrome

---
+++ source/next_smallest_palindrome.cpp
@@ -0,0 +1,11 @@
+/**
+* @file next_smallest_palindrome.cpp
+* @author Matan Levy (levymatanlevy@gmail.com)
+* @brief
+* @version 0.1
+* @date 2023-08-17
+*
+*
+*/
+#include <next_smallest_palindrome/next_smallest_palindrome.h>
+
---
+++ test/source/next_smallest_palindrome.cpp
@@ -0,0 +1,11 @@
+/**
+* @file next_smallest_palindrome.cpp
+* @brief
+* @version 0.1
+* @date 2023-08-17
+*
+*
+*/
+#include <doctest/doctest.h>
+#include <next_smallest_palindrome/next_smallest_palindrome.h>
+
